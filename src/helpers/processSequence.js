240b621ad80e6740c528929c3a4a93f37e2808db
Amal Dusalin <adusalin@fil-it.ru>
2021-11-10 00:39:55 +0300

    Решение для файла processSequence

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,106 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+import { compose, lt, prop, tap, gt, match, allPass, when, complement, andThen, curry, __ } from 'ramda';
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+const getValue = prop('value');
+const getLength = prop('length');
+const getHandleError = prop('handleError');
+const getResult = prop('result');
+const getWriteLog = prop('writeLog');
+const getHandleSuccess = prop('handleSuccess');
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+const isLessThenTen = gt(10);
+const isGreaterThenTwo = lt(2);
+const isGreaterThenZero = lt(0);
+const isNumber = match(/^[\d]+.{0,1}[\d]*$/);
+const isValueLengthValid = compose(
+    allPass([
+        isLessThenTen,
+        isGreaterThenTwo
+    ]),
+    getLength,
+)
+const isValueGreaterThanZero = compose(
+    isGreaterThenZero,
+    parseFloat
+)
+const isValueValid = compose(
+    allPass([
+        isValueLengthValid,
+        isValueGreaterThanZero,
+        isNumber,
+    ]),
+    getValue
+)
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+const parseIntFromValue = compose(
+    Math.round,
+    parseFloat,
+    getValue
+)
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+const getTech = api.get('https://api.tech/numbers/base')
+const getBinaryValue = (value) => getTech({from: 10, to: 2, number: value})
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+const getAnimalById = (id) => api.get(`https://animals.tech/${id}`)({})
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
+const logError = (data) => getHandleError(data)('Validation Error')
+
+const isValueNotValid = complement(isValueValid)
+
+const square = curry(Math.pow)(__, 2)
+const threeDelimeter = (value) => value % 3
+
+const processSequence = (data) => {
+
+    const writeLog = getWriteLog(data)
+    const handleSuccess = getHandleSuccess(data)
+
+    const logValue = compose(
+        writeLog,
+        getValue
+    )
+
+    const result = compose(
+        andThen(handleSuccess),
+        andThen(getResult),
+        andThen(getAnimalById),
+        andThen(tap(writeLog)),
+        andThen(threeDelimeter),
+        andThen(tap(writeLog)),
+        andThen(square),
+        andThen(tap(writeLog)),
+        andThen(getLength),
+        andThen(tap(writeLog)),
+        andThen(getResult),
+        getBinaryValue,
+        tap(writeLog),
+        parseIntFromValue,
+        when(
+            isValueNotValid,
+            tap(logError)
+        ),
+        tap(logValue)
+    )
+
+    result(data)
 }
 
 export default processSequence;
